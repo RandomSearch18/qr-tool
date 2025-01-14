f737c8af433bbd47801f2363cfb97b2a11e10bf5
RandomSearch <101704343+RandomSearch18@users.noreply.github.com>
2025-01-14 17:49:49 +0000

    show ans when click on ans

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ const $$ = <T extends Element>(selector: string) =>
 
 const database: Card[] = JSON.parse(localStorage.getItem("db") || "[]")
 
+let currentCard: number
+
 /**
  * Render a card to the DOM
  * @param i Index of the card
@@ -14,8 +16,9 @@ const database: Card[] = JSON.parse(localStorage.getItem("db") || "[]")
  */
 const showCard = (i: number): 0 | undefined => {
   if (!database[i]) return 0
-  $("h2").textContent = database[i][0]
-  $("p").textContent = database[i][1]
+  $("h2").textContent = `Card ${i + 1}/${database.length}`
+  $("p").textContent = database[i][0]
+  currentCard = i
 }
 
 // If there are cards in the database, show the first one & ensure that the current card elements are visible
@@ -29,4 +32,16 @@ window.n = () => {
   database.push([question, answer])
   localStorage.setItem("db", JSON.stringify(database))
   location.reload()
+}
+
+// @ts-ignore
+window.s = () => {
+  // Callback for when the "Show" (show answer) button is clicked
+  $("p").textContent = database[currentCard][1]
+}
+
+// @ts-ignore
+window.y = () => {
+  // Callback for the checkmark button
+  database.splice(currentCard, 1)
 }
