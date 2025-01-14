372ab6d0d962b17c2080ca6562e08c454583755b
RandomSearch <101704343+RandomSearch18@users.noreply.github.com>
2025-01-14 21:12:51 +0000

    Refine Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,18 @@
 import { defineConfig } from "vite"
 import { viteSingleFile } from "vite-plugin-singlefile"
 import { createHtmlPlugin } from "vite-plugin-html"
-import basicSsl from "@vitejs/plugin-basic-ssl"
 
 export default defineConfig({
   plugins: [
     viteSingleFile({
       removeViteModuleLoader: true,
-      useRecommendedBuildConfig: true,
-      inlinePattern: [],
     }),
     createHtmlPlugin({
       minify: true,
     }),
-    // basicSsl(),
   ],
   build: {
     cssCodeSplit: false,
     assetsInlineLimit: Infinity,
-  },
-  server: {
-    // https: true,
   },
 })
