5947214a0aa7cbd1237c465b50102b54148be668
RandomSearch <101704343+RandomSearch18@users.noreply.github.com>
2025-01-14 17:41:12 +0000

    do some flashcard things

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,23 @@ const $$ = <T extends Element>(selector: string) =>
 
 const database: Card[] = JSON.parse(localStorage.getItem("db") || "[]")
 
-database.length && $("a").removeAttribute("hidden")
+/**
+ * Render a card to the DOM
+ * @param i Index of the card
+ * @returns `undefined` if the card **was** shown without any errors, or `0` if the card doesn't exist
+ */
+const showCard = (i: number): 0 | undefined => {
+  if (!database[i]) return 0
+  $("h2").textContent = database[i][0]
+  $("p").textContent = database[i][1]
+}
+
+// If there are cards in the database, show the first one & ensure that the current card elements are visible
+showCard(0) ?? $("a").removeAttribute("hidden")
 
 // @ts-ignore
 window.n = () => {
+  // Callback for when the "+" (add card) button is clicked
   const question = $<HTMLTextAreaElement>("#q").value
   const answer = $<HTMLTextAreaElement>("#a").value
   database.push([question, answer])
